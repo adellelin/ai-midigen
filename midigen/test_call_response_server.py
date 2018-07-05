deba9a7be6e916932ba87591447bed480f13b112
adellelin <adelle.lin@gmail.com>
2018-07-05 15:05:51 -0700

    added midi file testing to crserver test

--- midigen/test_call_response_server.py
+++ midigen/test_call_response_server.py
@@ -9,26 +9,37 @@ import pretty_midi as pm
 # TODO: for now just manually launch a crserver with the --verbose_response flag to run this test
 crserver_url = 'http://127.0.0.1:5000/tfgen'
 this_dir = split(abspath(__file__))[0]
-test_file = join(this_dir, 'test_midi', '2Bar_120BPM_SET3_01.mid')
-output_dir = expanduser('~')
+test_file = join(this_dir, 'test_midi', 'Bass_valid_01.mid')
+#output_dir = expanduser('~')
+output_dir = '/Users/adelleli/Documents/Git/ai-midigen/midigen/test_midi/'
 
 midi = pm.PrettyMIDI(test_file)
 call_io = BytesIO()
 midi.write(call_io)
+midi.write(join(output_dir, 'call_midi_01.mid'))
 a = call_io.getvalue()
 r = requests.post(
     url=crserver_url,
     data=a,
     headers={'Content-Type': 'application/octet-stream'})
 
-if r.headers['content-type'] == 'application/json; charset=utf-8':
+#print(r.headers['content-type'])
+
+if r.headers['content-type'] == 'application/octet-stream':
+    response_io = BytesIO(r.content)
+    response_midi = pm.PrettyMIDI(response_io)
+    response_midi.write(join(output_dir, 'test_crserver_out-1.mid'))
+
+elif r.headers['content-type'] == 'application/json; charset=utf-8':
     response_dict = json.loads(r.content, encoding='utf-8')
     response_midi_bytes = base64.b64decode(response_dict['midi'])
     response_midi = pm.PrettyMIDI(BytesIO(response_midi_bytes))
     response_out_dist = response_dict['output_distribution']
     response_midi.write(join(output_dir, 'test_crserver_out.midi'))
+
 else:
     raise ValueError()
+
 
 # TODO: run crserver as subprocess? could use example from:
 #  https://stackoverflow.com/questions/16071866/non-blocking-subprocess-call
