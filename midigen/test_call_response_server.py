f711f5f419b240f6ffd500478d678a34b0d33fdf
adellelin <adelle.lin@gmail.com>
2019-05-09 09:08:34 -0700

    goes through a folder of midi files and inferences them instead of single file

--- midigen/test_call_response_server.py
+++ midigen/test_call_response_server.py
@@ -1,4 +1,5 @@
 from os.path import abspath, split, join, expanduser
+import os
 from io import BytesIO
 import requests
 import json
@@ -7,46 +8,57 @@ import pretty_midi as pm
 
 # TODO: make these parameters arguments and possibly launch a crserver as a secondary process
 # TODO: for now just manually launch a crserver with the --verbose_response flag to run this test
-crserver_url = 'http://127.0.0.1:6000/tfgen'
+crserver_url = 'http://127.0.0.1:5000/tfgen'
 this_dir = split(abspath(__file__))[0]
-test_file = join(this_dir, 'test_midi_jazz_guitar', 'call3.mid')
+#test_file = join(this_dir, 'test', 'full_call_61.mid') # full_call_8.mid, full_call_12.mid, full_call_37.mid, full_call_43.mid
 #output_dir = expanduser('~')
-output_dir = '/Users/adelleli/Documents/Git/ai-midigen/midigen/test_midi_jazz_guitar/'
+output_dir = '/Users/adelleli/Documents/Git/ai-midigen/midigen/R2_SET04_weighted_response/'
+directory = join(this_dir, 'R2_SET04_weighted_call')
+output_fname_prefix = 'R2_04_w_'
+print(directory)
 
-midi = pm.PrettyMIDI(test_file)
-call_io = BytesIO()
+for root, dirs, files in os.walk(directory):
+    print(root, dirs, files, directory)
+    for i, file in enumerate(files):
+        full_fname = join(directory, file)
+        print(full_fname)
+        if '.DS_Store' in full_fname:
+            continue
+        midi = pm.PrettyMIDI(full_fname)
 
-# validate the call
-midi.write(call_io)
-midi.write(join(output_dir, 'call_midi_3.mid'))
-a = call_io.getvalue()
-try:
-    r = requests.post(
-        url=crserver_url,
-        data=a,
-        headers={'Content-Type': 'application/octet-stream'})
-except IOError as e:
-    print(e)
+        call_io = BytesIO()
+        # validate the call
+        midi.write(call_io)
+        #midi.write(join(output_dir, full_fname))
+        a = call_io.getvalue()
+        try:
+            r = requests.post(
+                url=crserver_url,
+                data=a,
+                headers={'Content-Type': 'application/octet-stream'})
+        except IOError as e:
+            print(e)
 
+        print(r.headers['content-type'])
+        print(r)
 
-print(r.headers['content-type'])
-print(r)
+        if r.headers['content-type'] == 'application/octet-stream':
+            response_io = BytesIO(r.content)
+            response_midi = pm.PrettyMIDI(response_io)
+            output_fname = output_fname_prefix + str(i*2)+'.mid'
+            #response_midi.write(join(output_dir, 'imamura_06B.mid'))
+            print("output_midi", output_fname)
+            response_midi.write(join(output_dir, output_fname))
 
-if r.headers['content-type'] == 'application/octet-stream':
-    print("midi")
-    response_io = BytesIO(r.content)
-    response_midi = pm.PrettyMIDI(response_io)
-    response_midi.write(join(output_dir, 'test_out-3.mid'))
+        elif r.headers['content-type'] == 'application/json; charset=utf-8':
+            response_dict = json.loads(r.content, encoding='utf-8')
+            response_midi_bytes = base64.b64decode(response_dict['midi'])
+            response_midi = pm.PrettyMIDI(BytesIO(response_midi_bytes))
+            response_out_dist = response_dict['output_distribution']
+            response_midi.write(join(output_dir, 'test_crserver_out.midi'))
 
-elif r.headers['content-type'] == 'application/json; charset=utf-8':
-    response_dict = json.loads(r.content, encoding='utf-8')
-    response_midi_bytes = base64.b64decode(response_dict['midi'])
-    response_midi = pm.PrettyMIDI(BytesIO(response_midi_bytes))
-    response_out_dist = response_dict['output_distribution']
-    response_midi.write(join(output_dir, 'test_crserver_out.midi'))
-
-else:
-    raise ValueError()
+        else:
+            raise ValueError()
 
 
 # TODO: run crserver as subprocess? could use example from:
